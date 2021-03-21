38800c0673b7b93879463ff4ca6cd19af732aec8
seungwonleee <seungwon.code@gmail.com>
2021-03-22 00:08:57 +0900

    SDK 스크립트 경로 변경, README 수정

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-##설정 방법
+## 사용 방법
 
 ---
 
-1. fire_module 디렉토리에 fireMain.js 파일 생성
+1. src/fire_module 디렉토리에 fireSDK.js 생성
 
-2. firebase 프로젝트 설정에서 SDK 스크립트 firMain.js에 복사붙여넣기
+2. firebase 프로젝트 설정에서 SDK 스크립트를 복사해서 fireSDK.js 에 복사 붙여 넣기
 
-3. firebase 초기화
+3. SDK 스크립트 복사 붙여 넣기 후 export default firebaseConfig; 작성
 
-4. firMain.js에 auth 모듈 불러오고 export하기
-   ex) export const authService = firebase.auth();
+4. npm install 하여 dependencies 설치하기
+
+5. 프로젝트 실행
---
+++ src/fire_module/fireMain.js
@@ -0,0 +1,17 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use (사용하고 싶은 모듈 import 하기)
+import "firebase/auth";
+import "firebase/firestore";
+
+// For Firebase JS SDK Import
+import fireSDK from "./fireSDK";
+
+const firebaseConfig = fireSDK;
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
